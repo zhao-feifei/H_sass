71393b0a1105721e579f3b34342da28054f72afe
zhao-feifei <398983914@qq.com>
2022-02-04 15:04:43 +0800

    完成路由配置

---
+++ src/router/modules/departments.js
@@ -0,0 +1,18 @@
+import Layout from '@/layout'
+
+export default {
+  path: '/departments', // 路径
+  name: 'departments', // 给路由规则加一个name
+  component: Layout, // 组件
+  // 配置二级路的路由表
+  children: [
+    {
+      path: '', // 这里当二级路由的path什么都不写的时候 表示该路由为当前二级路由的默认路由
+      component: () => import('@/views/departments'),
+
+      meta: {
+        title: '组织架构'
+      }
+    }
+  ]
+}
