71c01ed6c7afe6f4d056f72a4d6db2b39b850bbd
zhao-feifei <398983914@qq.com>
2022-02-06 21:41:11 +0800

    头部完成

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>员工</h2>
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px"
+        >
+          <el-col>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span> 操作<i class="el-icon-arrow-down" /> </span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
@@ -10,4 +38,9 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
