b7f46bccb96ac051228d5ec7d2f8f46ecb101eee
zhao-feifei <398983914@qq.com>
2022-02-09 22:07:59 +0800

    员工导入完成

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -4,7 +4,7 @@
       <PageTools :show-before="true">
         <span slot="before">共{{ page.total }}条记录</span>
         <template slot="after">
-          <el-button size="small" type="warning">导入</el-button>
+          <el-button size="small" type="warning" @click="$router.push('/import')">导入</el-button>
           <el-button size="small" type="danger">导出</el-button>
           <el-button size="small" type="primary" @click="showDialog = true">新增员工</el-button>
         </template>
