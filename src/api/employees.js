5690bbc8057cac050a23e4268e5a0669d50a710b
zhao-feifei <398983914@qq.com>
2022-02-10 13:37:29 +0800

    密码修改功能完成

--- src/api/employees.js
+++ src/api/employees.js
@@ -51,3 +51,15 @@ export function importEmployee(data) {
     data
   })
 }
+
+/** *
+ *
+ * 保存员工的基本信息
+ * **/
+export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -5,15 +5,22 @@
         <el-tabs>
           <el-tab-pane label="登录账户设置">
             <!-- 放置表单 -->
-            <el-form label-width="120px" style="margin-left: 120px; margin-top:30px">
-              <el-form-item label="姓名:">
-                <el-input style="width:300px" />
+            <!-- 放置表单 -->
+            <el-form
+              ref="userForm"
+              :model="userInfo"
+              :rules="rules"
+              label-width="120px"
+              style="margin-left: 120px; margin-top:30px"
+            >
+              <el-form-item label="姓名:" prop="username">
+                <el-input v-model="userInfo.username" style="width:300px" />
               </el-form-item>
-              <el-form-item label="密码:">
-                <el-input style="width:300px" type="password" />
+              <el-form-item label="新密码:" prop="password2">
+                <el-input v-model="userInfo.password2" style="width:300px" type="password" />
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">更新</el-button>
+                <el-button type="primary" @click="saveUser">更新</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -26,8 +33,42 @@
 </template>
 
 <script>
+import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
 export default {
-
+  data() {
+    return {
+      userId: this.$route.params.id, // 这样可以后面直接通过 this.userId进行获取数据
+      userInfo: {
+        //   专门存放基本信息
+        username: '',
+        password2: ''
+      },
+      rules: {
+        username: [{ required: true, message: '姓名不能为空', trigger: 'blur' }],
+        password2: [{ required: true, message: '密码不能为空', trigger: 'blur' },
+        { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }]
+      }
+    }
+  },
+  created() {
+    this.getUserDetailById()
+  },
+  methods: {
+    async getUserDetailById() {
+      this.userInfo = await getUserDetailById(this.userId)
+    },
+    async saveUser() {
+      try {
+        // 校验
+        await this.$refs.userForm.validate()
+        await saveUserDetailById({ ...this.userInfo, password: this.userInfo.password2 }) // 将新密码的值替换原密码的值
+        this.$message.success('保存成功')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
 
