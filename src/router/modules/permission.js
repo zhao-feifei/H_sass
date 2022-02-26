6bfbab87d036fb959264104e00259ad77616284f
zhao-feifei <398983914@qq.com>
2022-02-26 08:40:49 +0800

    筛选出动态路由

--- src/router/modules/permission.js
+++ src/router/modules/permission.js
@@ -9,7 +9,6 @@ export default {
     {
       path: '', // 这里当二级路由的path什么都不写的时候 表示该路由为当前二级路由的默认路由
       component: () => import('@/views/permission'),
-
       meta: {
         title: '权限管理',
         icon: 'lock'
